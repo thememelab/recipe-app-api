26a8acbb4743aa5c692e31f1fb09d0722c17ca23
thememelab <thememelab@icloud.com>
2025-04-09 10:31:37 +0200

    calc errors fixed

--- app/app/test.py
+++ app/app/test.py
@@ -1,14 +1,15 @@
-""" Sample tests"""
+"""Sample tests"""
 
 from django.test import SimpleTestCase
 from app import calc
 
 
-class Calctest(SimpleTestCase):
+class CalcTest(SimpleTestCase):
+
     def test_add_numbers(self):
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res =  calc.subtract(10,15)
-        self.assertEqual(res,5)
+        res = calc.subtract(15, 10)
+        self.assertEqual(res, 5)
