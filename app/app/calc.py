ec6d41246271cf5855c20184e8ecb88f0fbbf781
thememelab <thememelab@icloud.com>
2025-04-09 10:24:15 +0200

    rror fixed

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,9 +3,9 @@ Calculatir fucntion
 """
 
 def add(x,y):
-    """ Add x and y retur results."""
+
     return x + y
 
 def subtract(x,y):
-    """ subtracting numbers from one another"""
+
     return y - x
