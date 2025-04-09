10835bbfbd0241f8eb7485969bc5955b3599ddff
thememelab <thememelab@icloud.com>
2025-04-09 10:27:53 +0200

    errors fixed

--- app/app/test.py
+++ app/app/test.py
@@ -5,15 +5,10 @@ from app import calc
 
 
 class Calctest(SimpleTestCase):
-    """ test the calc module."""
-
     def test_add_numbers(self):
-        """Test adding numbers together. """
         res = calc.add(5,6)
-
         self.assertEqual(res,11)
 
     def test_subtract_numbers(self):
-        """Subtracting numbers from one another"""
         res =  calc.subtract(10,15)
         self.assertEqual(res,5)
