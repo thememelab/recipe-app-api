a978e46dc4efffb09efee0996200831fa0ff52ce
thememelab <thememelab@icloud.com>
2025-04-14 11:36:30 +0200

    configured docker compose correctly

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--b-ul)y)j5l8gc&+(qcjjd=9zbwcdo=iyqr8rnvt+xxzeb+4x1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
